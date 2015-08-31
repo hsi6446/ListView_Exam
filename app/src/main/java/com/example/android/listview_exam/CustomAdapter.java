a1abea1480b3022b7a490b4cecf29ef8cf090909
Suin.heo <hsi6446@gmail.com>
2015-08-31 13:23:20 +0900

    adapter complete

--- app/src/main/java/com/example/android/listview_exam/CustomAdapter.java
+++ app/src/main/java/com/example/android/listview_exam/CustomAdapter.java
@@ -62,22 +62,53 @@ public class CustomAdapter extends BaseAdapter {
 
         //convertView 처음 로딩될 때 초기화 과정을 거쳐서 viewHolder 에 저장
         if(convertView == null) {
+
+            //레이아웃을 가져오기
             View rootView = LayoutInflater.from(mContext).inflate(R.layout.item_people, parent, false);
+
+            //각 View를 소스로 연결
+            ImageView imageView = (ImageView)convertView.findViewById(R.id.imageId);
+            TextView name = (TextView)convertView.findViewById((R.id.name));
+            TextView phoneNumber = (TextView)convertView.findViewById(R.id.phoneNumber);
+
+
+            //viewHolder 를 생성 후, 가져온 view 를 연결
+            viewHolder= new ViewHolder();
+            viewHolder.image = imageView;
+            viewHolder.name = name;
+            viewHolder.phone = phoneNumber;
+
+
+            //viewHolder 를 convertView 의 태그로 저장.
+            convertView.setTag(viewHolder);
+
+
         } else {
             //convertView 가 다시 로딩 될 때에는 viewHolder 에서 꺼내와서 재사용
             //이점 : 속도가 빠르다.
+
+            viewHolder = (ViewHolder)convertView.getTag();
         }
 
         // 2. 레이아웃에 데이터를 바인딩
 
+        People people = (People)getItem(position);
+
+//      둘 중 하나만 쓰면 됨.
+//  People people1 = mData.get(position);
+
+        viewHolder.image.setImageResource(people.getImageResourceId());
+        viewHolder.name.setText(people.getName());
+        viewHolder.phone.setText(people.getPhoneNumber());
+
         // 3. 완성된 view 를 리턴
 
-        return null;
+        return convertView;
     }
 
 
     static class ViewHolder {
-        ImageView imageView;
+        ImageView image;
         TextView name;
         TextView phone;
     }
