b5984cdf7dd2f95fb3a8660a82673a7965bce621
Suin.heo <hsi6446@gmail.com>
2015-08-31 14:45:18 +0900

    phone call setting

--- app/src/main/java/com/example/android/listview_exam/MainActivity.java
+++ app/src/main/java/com/example/android/listview_exam/MainActivity.java
@@ -1,9 +1,14 @@
 
 package com.example.android.listview_exam;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -49,6 +54,22 @@ public class MainActivity extends AppCompatActivity {
     private void initListView() {
         ListView mListView = (ListView) findViewById(R.id.list_view);
         mListView.setAdapter(mAdapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // item이 클릭 되었을 때 동작하는 부분
+                Toast.makeText(getApplicationContext(), "position : " + position, Toast.LENGTH_SHORT).show();
+
+                Uri uri = Uri.parse("tel: " + mData.get(position).getPhoneNumber());
+                Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+
+                startActivity(intent);
+
+
+            }
+        });
     }
+
 
 }
