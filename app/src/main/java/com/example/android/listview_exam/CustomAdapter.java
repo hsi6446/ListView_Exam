6428676e6dc8273442844f3d05c8e073f0fa957a
Suin.heo <hsi6446@gmail.com>
2015-08-31 11:55:01 +0900

    inflater practice

--- app/src/main/java/com/example/android/listview_exam/CustomAdapter.java
+++ app/src/main/java/com/example/android/listview_exam/CustomAdapter.java
@@ -2,9 +2,12 @@
 package com.example.android.listview_exam;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -15,9 +18,11 @@ import java.util.List;
 public class CustomAdapter extends BaseAdapter {
 
     private List<People> mData;
+    private Context mContext;
 
     public CustomAdapter(Context context, List<People> data) {
         mData = data;
+        mContext = context;
     }
 
 
@@ -50,7 +55,31 @@ public class CustomAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
+
+        // 1. 레이아웃 구성
+
+
+        //convertView 처음 로딩될 때 초기화 과정을 거쳐서 viewHolder 에 저장
+        if(convertView == null) {
+            View rootView = LayoutInflater.from(mContext).inflate(R.layout.item_people, parent, false);
+        } else {
+            //convertView 가 다시 로딩 될 때에는 viewHolder 에서 꺼내와서 재사용
+            //이점 : 속도가 빠르다.
+        }
+
+        // 2. 레이아웃에 데이터를 바인딩
+
+        // 3. 완성된 view 를 리턴
+
         return null;
+    }
+
+
+    static class ViewHolder {
+        ImageView imageView;
+        TextView name;
+        TextView phone;
     }
 
 
